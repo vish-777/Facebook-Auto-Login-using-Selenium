de925cea79dcb04c4c94224ff178326fb90949c9
vishvk <54065184+vish-777@users.noreply.github.com>
2020-09-14 16:04:11 +0400

    Update facebook_Selenium.py

--- facebook_Selenium.py
+++ facebook_Selenium.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import time
 
 username = 'fb_email@email.com'
@@ -6,7 +7,7 @@ password = 'fb_password'
 
 url = 'https://www.facebook.com/'
 
-driver = webdriver.Chrome("/Users/johnfisher/Downloads/chromedriver")
+driver = webdriver.Chrome("C:\Python\Python38\Scripts\chromedriver")
 
 driver.get(url)
 
@@ -16,4 +17,6 @@ driver.find_element_by_id('pass').send_keys(password)
 time.sleep(2)
 
 driver.find_element_by_id('loginbutton').click()
+
+driver.close()
 
